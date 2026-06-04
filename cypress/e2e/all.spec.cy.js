9ed87034f3ecbbf17679e9314310ddad759d8e58
Douglas Sampaio <douglassampaio23@gmail.com>
2026-06-04 15:20:14 -0300

    First commit: test Cypress Heroes

---
+++ cypress/e2e/all.spec.cy.js
@@ -0,0 +1,20 @@
+import userData from '../fixtures/users/userData.json'
+import LoginPage from '../pages/loginPage'
+
+
+const loginPage = new LoginPage()
+
+
+describe('Login com sucesso', () => {
+  it.only('Deve fazer login com um usuário válido', () => {
+    loginPage.accessLoginPage()
+    loginPage.accountLogin(userData.userSuccess.email, userData.userSuccess.password)
+  })
+})
+
+describe('Tentar fazer login com credenciais inválidas', () => {
+  it('Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas', () => {
+    loginPage.accessLoginPage()
+    loginPage.accountLogin(userData.userFailed.email, userData.userFailed.password)
+  })
+})
---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,32 @@
+class loginPage {
+  selectorsList() {
+    const selectors = {
+      'loginField': "[name='email']",
+      'passField': "[name='password']",
+      'buttonLogin': ".bg-blue-700",
+      'acessLogin': '.gap-8',
+      'errorText': '.text-red-500'
+    };
+    return selectors;
+  }
+  accessLoginPage(){
+        cy.visit('/heroes')
+  }
+
+  accountLogin(email, pass) {
+    cy.get(this.selectorsList().acessLogin).eq(0).click();
+    cy.get(this.selectorsList().loginField).type(email);
+    cy.get(this.selectorsList().passField).type(pass);
+    cy.get(this.selectorsList().buttonLogin).click();
+  }
+  accountLoginFailed(email, pass) {
+    cy.get(this.selectorsList().acessLogin).eq(0).click();
+    cy.get(this.selectorsList().loginField).type(email);
+    cy.get(this.selectorsList().passField).type(pass);
+    cy.get(this.selectorsList().buttonLogin).click();
+    cy.get(this.selectorsList().errorText).should('be.visible')
+  }
+
+}
+
+export default loginPage;
